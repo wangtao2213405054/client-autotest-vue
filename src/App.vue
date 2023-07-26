72d66f1c83af2bc2f68739de65b92443147763f7
Coke <tao.wang@davincimotor.com>
2023-07-26 14:06:36 +0800

    add beian

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
     <router-view />
+    <div
+      style="position:fixed;text-align:center;bottom:0;margin:0 auto;width:100%;color: #5c6b77"
+    >
+      <div style="margin-bottom: 10px">
+        <a
+          target="_blank"
+          style="color: #5c6b77"
+          href="https://beian.miit.gov.cn/"
+        >备案号: </a>
+        &nbsp;
+        <a
+          target="_blank"
+          style="color: #5c6b77"
+          href="https://beian.miit.gov.cn/"
+        >京ICP备2023003728号-1</a>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,56 +1,20 @@
 <template>
-  <div class="card">
-    <div class="card__arrow">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="15" width="15">
-        <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z" />
-      </svg>
-    </div>
+  <div>
+    <el-skeleton :throttle="300" animated>
+      <template slot="template">
+        <el-skeleton-item variant="button" style="width: 100%; height: 300px;" />
+        <div style="padding: 14px;">
+          <el-skeleton-item variant="button" style="width: 100%" />
+          <div
+            style="display: flex; align-items: center"
+          >
+            <el-skeleton-item variant="text" style="margin-right: 16px;" />
+            <el-skeleton-item variant="text" style="width: 30%;" />
+          </div>
+        </div>
+      </template>
+    </el-skeleton>
   </div>
 </template>
-
 <script>
-export default {
-}
 </script>
-
-<style lang="scss" scoped>
-/* this card is inspired form this - https://georgefrancis.dev/ */
-
-.card {
-  --border-radius: 0.75rem;
-  --primary-color: rgba(114, 87, 250, 0.01);
-  padding: 1rem;
-  border-radius: var(--border-radius);
-  position: relative;
-}
-
-.card > * + * {
-  margin-top: 1.1em;
-}
-
-.card .card__arrow {
-  position: absolute;
-  background: var(--primary-color);
-  padding: 0.4rem;
-  border-top-left-radius: var(--border-radius);
-  border-bottom-right-radius: var(--border-radius);
-  bottom: 0;
-  right: 0;
-  transition: 0.2s;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.card svg {
-  transition: 0.2s;
-}
-
-.card:hover .card__arrow {
-  background: #111;
-}
-
-.card:hover .card__arrow svg {
-  transform: translateX(3px);
-}
-</style>
